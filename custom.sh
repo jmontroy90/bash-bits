5c9e683b03122df728ac1cc25a0cd3dd5f8675df
John Montroy <jmontroy@theladders.com>
2016-10-16 02:14:10 -0400

    Initial

---
+++ custom.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## create directory and cd into it
+mkcd_fn() {
+        mkdir $1
+        cd $1
+}
+alias mkcd=mkcd_fn
+
+## define `up N` to cd up N directories
+function cd_fn() {
+  cd $(printf "%0.s../" $(seq 1 $1))
+}
+alias up=cd_fn
+
+## `mkdirs a b c` will create that directory structure without cd'ing into any of them
+function mkdirs_fn() {
+  itr=0
+  for dirs in "$@"; do # get all args
+    mkcd $dirs
+    itr=$((itr+1))
+  done
+  up $itr
+}
+alias mkdirs=mkdirs_fn
